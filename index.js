b6f3677d8b5716bc91060f86e7b416da33cff7fe
Julio Lopez <joeyclopez88#gmail.com>
2021-09-28 22:05:41 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// store data--where?
+
+
+
+class Timer {
+    constructor (durationInput,startButton,pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+    start = () => {
+        this.tick();
+       this.intervalId = setInterval(this.tick, 1000);
+      
+    }
+
+    pause = () => {
+        clearInterval(this.intervalId);
+    }
+
+    onDurationChange() {
+        
+    }
+    tick = () => {
+
+        
+        timeRemaining = timeRemaining - 1;
+        console.log('tick');
+    }
+}
+
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+
+const timer = new Timer(durationInput, startButton, pauseButton);
