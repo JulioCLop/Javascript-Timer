f3d125917bac81fc4b2478a4ce5e5b9ad2bc833c
Julio Lopez <joeyclopez88#gmail.com>
2021-10-03 11:57:34 -0700

    added animation to count down circle

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffSet = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
+
     onStart() {
         console.log('timer started');
 
     },
     onTick() {
-        console.log('Timer just ticked down')
+       
+        circle.setAttribute('stroke-dashoffset', currentOffSet)
+        currentOffSet = currentOffSet - 40;
     },
     onComplete() {
         console.log('Timer is complete');
